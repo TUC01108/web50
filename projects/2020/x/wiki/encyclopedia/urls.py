7082d6ed5fc05f23f0e25635bf0f66e878136bd5
tuc01108 <tuc01108@temple.edu>
2020-07-31 16:27:40 -0500

    urls update with wiki

--- projects/2020/x/wiki/encyclopedia/urls.py
+++ projects/2020/x/wiki/encyclopedia/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("wiki/", views.wiki, name="wiki"),
+    path("", views.index, name="index"),
 ]
--- projects/2020/x/wiki/encyclopedia/views.py
+++ projects/2020/x/wiki/encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from . import util
 
@@ -7,4 +8,7 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def wiki(request):
+    return HttpResponse("Hello, world. You're at the titles.")
 
