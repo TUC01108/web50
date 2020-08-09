4c997edf35d0b70c2306ffb431eb0e8e6208b6e8
tuc01108 <tuc01108@temple.edu>
2020-08-08 20:42:56 -0500

    entries appear on page

--- projects/wiki/encyclopedia/urls.py
+++ projects/wiki/encyclopedia/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
+    path("wiki/<entry_name>/", views.entry, name="entry"),
     path("wiki/", views.wiki, name="wiki"),
     path("", views.index, name="index"),
 ]
--- projects/wiki/encyclopedia/views.py
+++ projects/wiki/encyclopedia/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from . import util
+import datetime
 
 
 def index(request):
@@ -12,3 +13,6 @@ def index(request):
 def wiki(request):
     return HttpResponse("Hello, world. You're at the titles.")
 
+def entry(request, entry_name):
+    #html = "<html><body>%s</body></html>"
+    return HttpResponse(util.get_entry(entry_name))
